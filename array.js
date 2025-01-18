b9fc6f3c3940ed51fd620018afb884ac75a78770
panchal vatsal <161142879+panchalvatsal2512@users.noreply.github.com>
2025-01-18 13:58:21 +0530

    Update array.js

--- array.js
+++ array.js
@@ -1,51 +1,53 @@
-// array ni anadar last value add karva mate push no use thay
+1.array ni anadar last value add karva mate push no use thay
 
-// let personname = ["vatsal","manish","keyur","yash"];
-// personname.push("ahish");
-// console.log(personname);
+let personname = ["vatsal","manish","keyur","yash"];
+personname.push("ahish");
+console.log(personname);
 
-// array ni anadar last value remove karva mate pop no use thay
+2.array ni anadar last value remove karva mate pop no use thay
 
-// let personname = ["vatsal","yash"];
-// personname.pop();
-// console.log(personname);
+let personname = ["vatsal","yash"];
+personname.pop();
+console.log(personname);
 
-// shif thi array ma first value remove thase
+3.shif thi array ma first value remove thase
 
-// let personname = ["vatsal","manish","keyur","yash"];
-// personname.shift();
-// console.log(personname);
+let personname = ["vatsal","manish","keyur","yash"];
+personname.shift();
+console.log(personname);
 
-// unshif thi array ma first value add thase
+4.unshif thi array ma first value add thase
 
-// let personname = ["vatsal","manish","keyur","yash"];
-// personname.unshift("rahul");
-// console.log(personname);
+let personname = ["vatsal","manish","keyur","yash"];
+personname.unshift("rahul");
+console.log(personname);
 
-// array ni andar total item na count mate length no use thay
+5.array ni andar total item na count mate length no use thay
 
-// let personname = ["vatsal","manish","keyur","yash"];
-// console.log(personname.length);
-
-
-// array ni andar middle value ne remove karva mate splice method no use thay
-
-// let fruits = ["apple", "banana", "charry", "mango", "watermalon"];
-// console.log("original array:"+fruits);
-// fruits.splice(2,1)
-// console.log(fruits);
+let personname = ["vatsal","manish","keyur","yash"];
+console.log(personname.length);
 
 
-// array ni andar koi pan value ne add karva mate aa mathod no use thay 
+6.array ni andar middle value ne remove karva mate splice method no use thay
 
-// let fruits = ["apple", "banana", "charry", "mango", "watermalon"];
-// fruits.splice(1,1,"vatsal")
-// console.log(fruits);
+let fruits = ["apple", "banana", "charry", "mango", "watermalon"];
+console.log("original array:"+fruits);
+fruits.splice(2,1)
+console.log(fruits);
 
 
-// index ni adar ni value ne change karva mate aa mehtod no use thay
+7.array ni andar koi pan value ne add karva mate aa mathod no use thay 
 
-// let personname = ["vatsal","yash","manish","rahul"]
-// personname[2]=20
-// console.log(personname);
+let fruits = ["apple", "banana", "charry", "mango", "watermalon"];
+fruits.splice(1,1,"vatsal")
+console.log(fruits);
+
+
+8.index ni adar ni value ne change karva mate aa mehtod no use thay
+
+let personname = ["vatsal","yash","manish","rahul"]
+personname[2]=20
+console.log(personname);
+
+
 
