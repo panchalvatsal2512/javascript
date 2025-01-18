7038afffc23f3c7c0d42d7da90a7e4713f2fd209
panchal vatsal <161142879+panchalvatsal2512@users.noreply.github.com>
2025-01-18 13:54:07 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,51 @@
+// array ni anadar last value add karva mate push no use thay
+
+// let personname = ["vatsal","manish","keyur","yash"];
+// personname.push("ahish");
+// console.log(personname);
+
+// array ni anadar last value remove karva mate pop no use thay
+
+// let personname = ["vatsal","yash"];
+// personname.pop();
+// console.log(personname);
+
+// shif thi array ma first value remove thase
+
+// let personname = ["vatsal","manish","keyur","yash"];
+// personname.shift();
+// console.log(personname);
+
+// unshif thi array ma first value add thase
+
+// let personname = ["vatsal","manish","keyur","yash"];
+// personname.unshift("rahul");
+// console.log(personname);
+
+// array ni andar total item na count mate length no use thay
+
+// let personname = ["vatsal","manish","keyur","yash"];
+// console.log(personname.length);
+
+
+// array ni andar middle value ne remove karva mate splice method no use thay
+
+// let fruits = ["apple", "banana", "charry", "mango", "watermalon"];
+// console.log("original array:"+fruits);
+// fruits.splice(2,1)
+// console.log(fruits);
+
+
+// array ni andar koi pan value ne add karva mate aa mathod no use thay 
+
+// let fruits = ["apple", "banana", "charry", "mango", "watermalon"];
+// fruits.splice(1,1,"vatsal")
+// console.log(fruits);
+
+
+// index ni adar ni value ne change karva mate aa mehtod no use thay
+
+// let personname = ["vatsal","yash","manish","rahul"]
+// personname[2]=20
+// console.log(personname);
+
