8324cb664424e9c5ce8ce8234f7088fe8e826e4a
panchal vatsal <161142879+panchalvatsal2512@users.noreply.github.com>
2025-01-22 23:06:34 +0530

    Update conversions.js

--- conversions.js
+++ conversions.js
@@ -1,17 +1,17 @@
-// "33" => 33
-// "33abc" => NaN
-// true => 1
-// false => 0
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
 
-// 1=> true;
-// 0=> false;
-// "vatsal" => true;
-// "" => false
+1=> true;
+0=> false;
+"vatsal" => true;
+"" => false
 
 
 // ***********************Conversions**************************//
 
-// Conversion ka matlab hai kisi value ko ek data type se doosre data type mein badalna
+Conversion ka matlab hai kisi value ko ek data type se doosre data type mein badalna
 
 // example 1 : number to String
 let score = 33;
@@ -25,7 +25,6 @@ console.log(typeof numbertostring);
 
 
 //example 2:string to number
-
 let total = "33"
 console.log(total);
 console.log(typeof total);  
@@ -33,7 +32,6 @@ console.log(typeof total);
 let stringtonumber = Number(total)
 console.log(stringtonumber);
 console.log(typeof stringtonumber);
-
 
 
 // example 3:
@@ -45,8 +43,6 @@ console.log(logintostring);
 console.log(typeof logintostring);
 
 // example 4:
-
-
 let user = "vatsal";
 console.log(user);
 console.log(typeof user);
