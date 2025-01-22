a1e359dc30febf9a1f056efe6559a661ef60b749
panchal vatsal <161142879+panchalvatsal2512@users.noreply.github.com>
2025-01-22 23:09:41 +0530

    Update datatype-summer.js

--- datatype-summer.js
+++ datatype-summer.js
@@ -1,27 +1,26 @@
-// primitive datatypes // call by value
+primitive datatypes // call by value
 
-// 7 types : string,number,boolean,null,undefined,symbol,BigInt
+7 types : string,number,boolean,null,undefined,symbol,BigInt
 
+Non-primitive  // Referrence type
 
-// Non-primitive  // Referrence type
+Array,Objects,Functions
 
-//Array,Objects,Functions
+Function ka datatype function hota he but usko object  function bola jata he
 
-// Function ka datatype function hota he but usko object  function bola jata he
+return type of variables in javascript
 
-// return type of variables in javascript
+1)primitive datatypes
 
-// 1)primitive datatypes
+Number => Number
+String => String
+Boolean => Boolean
+null => Object
+Symbol => Symbol
+BigInt =>BigInt
 
-// Number => Number
-// String => String
-// Boolean => Boolean
-// null => Object
-// Symbol => Symbol
-// BigInt =>BigInt
+2) non-primitive datatype
 
-//2) non-primitive datatype
-
-// Arry => Object
-// Function => Function(object)
-// Object => Object
+Arry => Object
+Function => Function(object)
+Object => Object
